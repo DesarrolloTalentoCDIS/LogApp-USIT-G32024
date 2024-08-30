3f0027920b356e2b6fe0942e8a42ae1bc40c68ec
Denzel Pérez <denzelpeverax07@gmail.com>
2024-08-30 12:06:37 -0600

    14 Configurar el routing
    Configurar el routing principal para las rutas

--- LogAppG3/src/app/app-routing.module.ts
+++ LogAppG3/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./protected-app/protected-app.module').then(m => m.ProtectedAppModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule 
+  ]
 })
 export class AppRoutingModule { }
