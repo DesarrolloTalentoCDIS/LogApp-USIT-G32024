f7625f6f93a982924904847a7fcb56991cc5885b
Rodrigo Mtz <rodrigomtzmacias@hotmail.com>
2024-08-30 11:24:24 -0600

    11 Generar componentes para auth
    Generar componentes: login, main y register.

--- LogAppG3/src/app/auth/auth.module.ts
+++ LogAppG3/src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
