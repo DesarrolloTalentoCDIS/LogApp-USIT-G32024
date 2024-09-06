d4622a473721fa2d07c21603a01612a32661e570
LuisDiaz-ipsilon <54384617+LuisDiaz-ipsilon@users.noreply.github.com>
2024-09-06 10:32:33 -0500

    refactor login comportamiento
    se reafactoriza el comportamiento del login

--- LogAppG3/src/app/auth/components/main/main.component.ts
+++ LogAppG3/src/app/auth/components/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+
+  formularioLog : FormGroup = this.fb.group({
+    id: ['', [Validators.required, Validators.minLength(8)]],
+    pass: ['', [Validators.required, Validators.minLength(7)]]
+  });
+
+  constructor(private fb : FormBuilder,
+    private router : Router,
+    private authService : AuthService) { }
+
+    login(){
+      console.log(this.formularioLog.value);
+      console.log(this.formularioLog.valid);
+      this.router.navigateByUrl('/dashboard');
+    }
+
 
 }
